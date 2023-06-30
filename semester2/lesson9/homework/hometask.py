a3cf56f8af0c299f82ec20df23ca0e14ccb46c6d
Rimmyraa <127656391+Rimmyraa@users.noreply.github.com>
2023-06-30 20:47:49 +0300

    Update hometask.py

--- semester2/lesson9/homework/hometask.py
+++ semester2/lesson9/homework/hometask.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get('http://yermilovcentre.org/medias/')
-soup = BeautifulSoup(response.content, "html.parser")
 
-for media in soup.select('.row-fluid.row.no-gutters > col-6.px-0'):
+soup = BeautifulSoup(response.content, 'html.parser')
+
+for media in soup.select('.row title-text'):
     print(media.text)
